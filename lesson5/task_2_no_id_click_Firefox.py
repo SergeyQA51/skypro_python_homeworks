9d67ad1faa742acc2f0f2dbe64f05fd4ee68d23a
SergeyQA51 <sukmanovinstfp@mail.ru>
2024-07-20 11:41:54 +0300

    add fifth(2) homework

---
+++ lesson5/task_2_no_id_click_Firefox.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import sys
+
+# Функция для выполнения теста
+def run_test(browser):
+    
+    if browser == 'firefox':
+        driver = webdriver.Firefox()
+
+    try:
+        # Переход на страницу
+        driver.get("http://uitestingplayground.com/dynamicid")
+
+        # Клик по синеей кнопке
+        blue_button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
+        blue_button.click()
+        sleep(1) 
+    finally:
+        # Закрываем драйвер
+        driver.quit()
+
+# Запускаем тест 3 раза
+
+for _ in range(3):
+    print("Запуск теста в Firefox...")
+    run_test('firefox')
+    print("Тест завершен для Firefox.\n")
+
