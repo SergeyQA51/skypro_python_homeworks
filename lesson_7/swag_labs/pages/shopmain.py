586ec53d22a841e065480bd52b706626cd15db4c
SergeyQA51 <sukmanovinstfp@mail.ru>
2024-08-16 19:37:50 +0300

    add seventh(3) homework

--- lesson_7/swag_labs/pages/shopmain.py
+++ lesson_7/swag_labs/pages/shopmain.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from lesson_7.constants import Shop_URL
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class ShopmainPage:
     def __init__(self, browser):
@@ -11,12 +12,17 @@ class ShopmainPage:
         self._name = (By.ID, "user-name")
         self._pass = (By.ID, "password")
         self._log_button = (By.ID, "login-button")
-        self.browser.find_element(*self._name).send_keys("standart_user")
+        self.browser.find_element(*self._name).send_keys("standard_user")
         self.browser.find_element(*self._pass).send_keys("secret_sauce")
         self.browser.find_element(*self._log_button).click()
+        
+        WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.ID, "add-to-cart-sauce-labs-backpack"))
+        )
+       
 
     def buy_issue(self):
-        self.Sauce_Labs_Backpack = (By.ID, "add-to-cart-sauce-labs-backpack")
+        self.Sauce_Labs_Backpack = (By.ID, "add-to-cart-sauce-labs-backpack") 
         self.Sauce_Labs_Bolt_Tshirt = (By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
         self.Sauce_Labs_Onesie = (By.ID, "add-to-cart-sauce-labs-onesie")
 
